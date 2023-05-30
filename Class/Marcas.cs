4475b8459863583e7eb9269dd7c3bf2ae28be7d1
Fadriano91 <fquimico@hotmail.com.br>
2023-05-29 23:44:06 -0300

    conexao-banco-criado-&-criando-classes-marcas-carros

---
+++ Class/Marcas.cs
@@ -0,0 +1,75 @@
+﻿using LojaCar.Conn;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaCar.Class
+{
+    internal class Marcas
+    {
+        private int id;
+        private string marca;
+
+        public int Id { get => id; set => id = value; }
+        public string Marca { get => marca; set => marca = value; }
+
+        public static string salvar(Marcas m)
+        {
+            try
+            {
+                MySqlConnection conn = Conexao.obterConexao();
+                string sql = "INSERT INTO marcas VALUES (null, @marca)";
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@marca", m.Marca);
+                cmd.ExecuteNonQuery();
+                return "Categoria salva com sucesso";
+            }
+            catch (Exception ex)
+            {
+                return "erro: " + ex.Message;
+            }
+        }
+
+        public static string listar()
+        {
+            MySqlConnection conn = Conexao.obterConexao();
+            string sql = "SELECT * FROM marcas";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            MySqlDataReader res = cmd.ExecuteReader();
+            string lista = "Lista de marcas \n";
+            if (res.HasRows) 
+            {
+                while (res.Read())
+                {
+                    lista += "ID: " + res["id"].ToString();
+                    lista += " - Marca: " + res["marca"].ToString();
+                    lista += "\n";
+                }
+            }
+            return lista;
+        }
+
+        public static Boolean pesquisar(Marcas m)
+        {
+            MySqlConnection conn = Conexao.obterConexao();
+            string sql = "SELECT mar.* FROM marcas mar WHERE mar.id = @id";
+            MySqlCommand cmd = new MySqlCommand(sql, conn);
+            cmd.Parameters.AddWithValue("@id", m.Id);
+            MySqlDataReader res = cmd.ExecuteReader();
+            Boolean verificar = false;
+            if (res.HasRows)
+            {
+                while (res.Read())
+                {
+                    m.Id = Int32.Parse(res["id"].ToString());
+                    m.Marca = res["marca"].ToString();
+                    verificar = true;
+                }
+            }
+            return verificar;
+        }
+    }
+}
---
+++ Conn/Conexao.cs
@@ -0,0 +1,32 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LojaCar.Conn
+{
+    class Conexao
+    {
+        private static MySqlConnection conn = null;
+
+        public static MySqlConnection obterConexao()
+        {
+            conn = new MySqlConnection
+                ("Server=localhost;Database=loja;Uid=root;PWD=;");
+
+            try
+            {
+                conn.Open();
+            }
+            catch(Exception ex) 
+            {
+                conn = null;
+                MessageBox.Show(ex.Message);
+            }
+            return conn;
+        }
+    }
+}
